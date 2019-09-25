38ec9b94dd04a6ec86ca4cb4d06f6f59270e52af
Utku Bulkan <bulkanutku@gmail.com>
2019-09-25 21:52:45 +0100

    Rephrased scripts for all platforms including linux, macos, raspberry pi3 & 4

--- scripts/raspberry-pi4/raspberry-pi4-checkout.sh
+++ scripts/raspberry-pi4/raspberry-pi4-checkout.sh
@@ -1,23 +1,10 @@
-udo dphys-swapfile swapoff
-sudo sed -i 's:CONF_SWAPSIZE=.*:CONF_SWAPSIZE=2048:g' /etc/dphys-swapfile
-sudo reboot
-
-
 sudo apt-get update
-sudo apt-get install build-essential git cmake pkg-config libgtk2.0-dev
-
-# Clean build directories
-rm -rf opencv/build
-rm -rf opencv_contrib/build
+sudo apt-get -y install build-essential git cmake pkg-config libgtk2.0-dev
 
 sudo apt-get -y purge wolfram-engine
 sudo apt-get -y purge libreoffice*
 sudo apt-get -y clean
 sudo apt-get -y autoremove
-
-# Create directory for installation
-mkdir installation
-mkdir installation/OpenCV-"$cvVersion"
 
 sudo apt-get -y remove x264 libx264-dev
  
@@ -29,9 +16,10 @@ sudo apt-get -y install libtiff5-dev
 sudo apt-get -y install libtiff-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
 sudo apt-get -y install libxine2-dev libv4l-dev
-cd /usr/include/linux
-sudo ln -s -f ../libv4l1-videodev.h videodev.h
-cd $cwd
+
+# cd /usr/include/linux
+# sudo ln -s -f ../libv4l1-videodev.h videodev.h
+# cd $cwd
 
 sudo apt-get -y install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
 sudo apt-get -y install libgtk2.0-dev libtbb-dev qt5-default
@@ -47,40 +35,24 @@ sudo apt-get -y install libprotobuf-dev protobuf-compiler
 sudo apt-get -y install libgoogle-glog-dev libgflags-dev
 sudo apt-get -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
 
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install libatlas-base-dev gfortran
+
+sudo apt-get -y install qt5-default
+sudo apt-get -y install qtcreator
 
 git clone https://github.com/opencv/opencv.git
 cd opencv
-git checkout 3.4
+git checkout master
 cd ..
  
 git clone https://github.com/opencv/opencv_contrib.git
 cd opencv_contrib
-git checkout 3.4
+git checkout master
 cd ..
 
 cd opencv
 mkdir build
 cd build
-
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-            -D CMAKE_INSTALL_PREFIX=/usr/local \
-            -D INSTALL_C_EXAMPLES=ON \
-            -D WITH_TBB=ON \
-            -D WITH_V4L=ON \
-            -D WITH_QT=ON \
-            -D WITH_OPENGL=ON \
-            -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-            -D WITH_TBB=OFF \
-            -D BUILD_TBB=ON \
-            -D BUILD_EXAMPLES=ON ..
-
-make -j$(nproc)
-make install
-
-sudo sed -i 's/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
-sudo /etc/init.d/dphys-swapfile stop
-sudo /etc/init.d/dphys-swapfile start
---
+++ scripts/raspberry-pi4/raspberry-pi4-preinstall.sh
@@ -0,0 +1,5 @@
+sudo dphys-swapfile swapoff
+sudo sed -i 's:CONF_SWAPSIZE=.*:CONF_SWAPSIZE=2048:g' /etc/dphys-swapfile
+sudo reboot
+
+
